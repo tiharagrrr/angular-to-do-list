94f2625a2ab592fb24faed4587d43aad9c950484
Tihara Egodage <tiharaeg@gmail.com>
2024-06-15 02:10:10 +0530

    add task initial

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -3,11 +3,12 @@ import { TaskService } from '../../services/task.service';
 import { Task } from '../../Task';
 import { NgFor } from '@angular/common';
 import { TaskItemComponent } from '../task-item/task-item.component';
+import { AddTaskComponent } from '../add-task/add-task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [NgFor, TaskItemComponent],
+  imports: [NgFor, TaskItemComponent, AddTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
